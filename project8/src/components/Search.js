83fc041714636dfa510e1d6ca461920ac9bd500e
jackyyeh44 <jackyyeh55@gmail.com>
2024-12-24 22:16:19 +0800

    展示圖片
    展示圖片

--- project8/src/components/Search.js
+++ project8/src/components/Search.js
@@ -1,15 +1,6 @@
-import React from "react";
-import axios from "axios";
+import React, { useState } from "react";
 
-const Search = () => {
-  const auth = "p7ev32vx8W8dNG0n9R9L2hBgSDYAKdakyJMJp5QNBWVMv40gTuo3Y3LW";
-  const initialURL = "https://api.pexels.com/v1/curated?page=1&per_page=15";
-  const search = async () => {
-    let result = await axios.get(initialURL, {
-      headers: { Authorization: auth },
-    });
-    console.log(result);
-  };
+const Search = ({ search }) => {
   return (
     <div className="search">
       <input className="input" type="text" />
--- project8/src/pages/Homepage.js
+++ project8/src/pages/Homepage.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Search from "../components/Search";
+import Picture from "../components/Picture";
 
 const Homepage = () => {
+  let [data, setData] = useState(null);
+  const auth = "p7ev32vx8W8dNG0n9R9L2hBgSDYAKdakyJMJp5QNBWVMv40gTuo3Y3LW";
+  const initialURL = "https://api.pexels.com/v1/curated?page=1&per_page=15";
+
+  const search = async () => {
+    let result = await axios.get(initialURL, {
+      headers: { Authorization: auth },
+    });
+
+    setData(result.data.photos);
+  };
+
   return (
     <div style={{ minHeight: "100vh" }}>
-      <Search />
+      <Search search={search} />
+      <div className="picture">
+        {data &&
+          data.map((d) => {
+            return <Picture data={d} />;
+          })}
+      </div>
     </div>
   );
 };
