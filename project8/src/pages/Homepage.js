caa21796784900d38692b28a4189097e8192012a
jackyyeh44 <jackyyeh55@gmail.com>
2024-12-24 23:43:26 +0800

    更多圖片的功能
    更多圖片的功能

--- project8/src/pages/Homepage.js
+++ project8/src/pages/Homepage.js
@@ -2,12 +2,12 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Search from "../components/Search";
 import Picture from "../components/Picture";
-// import "../styles/style.css";
 
 const Homepage = () => {
   let [input, setInput] = useState("");
   let [data, setData] = useState(null);
   let [page, setPage] = useState(1);
+  let [currentSearch, setCurrentSearch] = useState("");
   const auth = "p7ev32vx8W8dNG0n9R9L2hBgSDYAKdakyJMJp5QNBWVMv40gTuo3Y3LW";
   const initialURL = "https://api.pexels.com/v1/curated?page=1&per_page=15";
   let searchURL = `https://api.pexels.com/v1/search?query=${input}&per_page=15&page=1`;
@@ -17,12 +17,25 @@ const Homepage = () => {
     });
 
     setData(result.data.photos);
+    setCurrentSearch(input);
   };
 
-  //   const morePicture = () => {
-  // let newURL;
-  // if()
-  //   };
+  const morePicture = async () => {
+    let newURL;
+    setPage(page + 1);
+    if (currentSearch === "") {
+      newURL = `https://api.pexels.com/v1/curated?page=${page + 1}&per_page=15`;
+    } else {
+      newURL = `https://api.pexels.com/v1/search?query=${currentSearch}&per_page=15&page=${
+        page + 1
+      }`;
+    }
+
+    let result = await axios.get(newURL, {
+      headers: { Authorization: auth },
+    });
+    setData(data.concat(result.data.photos));
+  };
 
   useEffect(() => {
     search(initialURL);
@@ -42,9 +55,9 @@ const Homepage = () => {
             return <Picture data={d} />;
           })}
       </div>
-      {/* <div className="morePicture">
+      <div className="morePicture">
         <button onClick={morePicture}>更多圖片</button>
-      </div> */}
+      </div>
     </div>
   );
 };
